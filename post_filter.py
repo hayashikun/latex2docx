2192af33867df690e742fe656dd0699581997a44
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-10-23 14:41:36 +0900

    update latex2docx

---
+++ post_filter.py
@@ -0,0 +1,28 @@
+import re
+
+import panflute as pf
+
+eq_label_numbers = dict()
+quad_re = re.compile(r"\\qquad\((\d+)\)", re.MULTILINE)
+eq_ref_re = re.compile(r"\[(eq:.+?)]")
+
+
+def make_index(elm, _):
+    if isinstance(elm, pf.Span) \
+            and isinstance(elm.content[0], pf.Math) \
+            and elm.content[0].format == "DisplayMath" \
+            and elm.identifier.startswith("eq:") \
+            and (m := quad_re.search(elm.content[0].text)) is not None:
+        eq_label_numbers[elm.identifier] = m.group(1)
+
+
+def update_eq_label(elm, _):
+    if isinstance(elm, pf.Link) \
+            and isinstance(elm.content[0], pf.Str) \
+            and (m := eq_ref_re.search(elm.content[0].text)) is not None:
+        elm.content[0].text = eq_label_numbers[m.group(1)]
+        return elm
+
+
+if __name__ == "__main__":
+    pf.run_filters([make_index, update_eq_label])
---
+++ pre_filter.py
@@ -0,0 +1,46 @@
+import re
+
+import panflute as pf
+
+
+def replace(elm, _):
+    if hasattr(elm, "text"):
+        updated = False
+        for k, v in {
+            r"\Im": r"\mathrm{Im}"
+        }.items():
+            if k in elm.text:
+                elm.text = elm.text.replace(k, v)
+                updated = True
+
+        if updated:
+            return elm
+
+
+def aligned_block(elm, _):
+    if isinstance(elm, pf.Math):
+        if elm.format == "DisplayMath" and elm.text.startswith(r"\begin{aligned}"):
+            text = elm.text.replace(r"\begin{aligned}", "").replace(r"\end{aligned}", "").replace("\n", "")
+            elms = list()
+            for eq in text.split(r"\\"):
+                elms.append(pf.Math(eq.replace("&", "")))
+                elms.append(pf.SoftBreak())
+
+            return elms[:-1]
+
+
+eq_label_re = re.compile(r"\\label{(eq:.+?)}", re.MULTILINE)
+
+
+def extract_eq_label(elm, _):
+    if isinstance(elm, pf.Math):
+        if elm.format == "DisplayMath" and (m := eq_label_re.search(elm.text)):
+            return [
+                pf.Math(elm.text.replace(m.group(0), "")),
+                pf.Space(),
+                pf.Str(f"{{#{m.group(1)}}}")
+            ]
+
+
+if __name__ == "__main__":
+    pf.run_filters([replace, aligned_block, extract_eq_label])
